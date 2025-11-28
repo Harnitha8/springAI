3f69336cbb23e239fca197117249624bf5a76def
Harnitha Sherupally <harnitha.sherupally@in.techwave.net>
2025-11-28 10:21:59 +0530

    minor changes

--- src/main/java/com/learning/springAI/tools/BasicTools.java
+++ src/main/java/com/learning/springAI/tools/BasicTools.java
@@ -6,6 +6,6 @@ import org.springframework.ai.tool.annotation.Tool;
 public class BasicTools {
     @Tool
     public int addNumbersPlusTen(int a, int b) {
-        return a + b + 10;
+        return a + b + 100;
     }
 }
